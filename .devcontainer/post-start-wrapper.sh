028abcdd1052feec23e8de9f158df4b4737de652
Marian Rudzynski <marian@rudzyn.ski>
2025-11-30 21:10:10 +0000

    chore: remove .devcontainer configuration

--- .devcontainer/post-start-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Log script execution for debugging
-echo "Running post-start script: $(date)" >> /tmp/post-start.log
-exec > >(tee -a /tmp/post-start.log)
-exec 2>&1
-
-bash .devcontainer/post-start.sh
